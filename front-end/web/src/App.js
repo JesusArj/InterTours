7862ff0cd7b2862f4b754ebfc1df12dab0e2d491
JesusArj <jesusarj01@gmail.com>
2023-04-04 00:10:58 +0200

    First ReactJS steps

--- front-end/web/src/App.js
+++ front-end/web/src/App.js
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Cookies from 'universal-cookie';
 
 function App() {
+
+
+  const [jwt, setJwt] = useState(""); 
+
+
+  useEffect(() => {
+    console.log('Hello World!');
+    const requestBody = {
+    username : "jesusarj",
+    password : "asdfasdf",
+  };
+
+  fetch("auth/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(requestBody),
+  })
+  .then(() => {
+    const cookies = new Cookies();
+    setJwt(cookies.get("jwt"));
+  });}
+  , [jwt])
+
   return (
-    <div className="App"> <h1> Hello World! </h1> </div>
+    <div className="App"> <h1> Hello World! </h1> 
+    <div> JWT Value is {jwt} </div>
+    
+    </div>
+    
   );
 }
 
