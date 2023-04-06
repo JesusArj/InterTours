e7cabbc8ed3c64cdad635772e65580e266a2e8e1
JesusArj <jesusarj01@gmail.com>
2023-04-06 14:24:45 +0200

    Add Login and PrivateRoute

--- front-end/web/src/App.js
+++ front-end/web/src/App.js
@@ -5,38 +5,46 @@ import { Route, Routes } from "react-router-dom";
 import { useLocalState } from "./util/useLocalStorage";
 import Dashboard from "./Dashboard";
 import Homepage from "./Homepage";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
-
   const [jwt, setJwt] = useLocalState("", "jwt");
 
-  useEffect(() => {
-    if (!jwt) {
-      console.log("Hello World!");
-      const requestBody = {
-        username: "jesusarj",
-        password: "asdfasdf",
-      };
+  // useEffect(() => {
+  //   if (!jwt) {
+  //     console.log("Hello World!");
+  //     const requestBody = {
+  //       username: "jesusarj",
+  //       password: "asdfasdf",
+  //     };
 
-      fetch("auth/login", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(requestBody),
-      }).then(() => {
-        const cookies = new Cookies();
-        setJwt(cookies.get("jwt"));
-      });
-    }
-  }, []);
+  //     fetch("auth/login", {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       method: "POST",
+  //       body: JSON.stringify(requestBody),
+  //     }).then(() => {
+  //       const cookies = new Cookies();
+  //       setJwt(cookies.get("jwt"));
+  //     });
+  //   }
+  // }, []);
 
   return (
     <Routes>
-      <Route path="/dashboard" element={ <Dashboard/> }/> 
-      <Route path="/" element={ <Homepage/> } />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Homepage />} />
     </Routes>
-
   );
 }
 
