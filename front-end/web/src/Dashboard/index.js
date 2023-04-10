c0991865523283b762fa4c00722ef9424b701f45
JesusArj <jesusarj01@gmail.com>
2023-04-10 23:15:03 +0200

    Add SideBar and Header to fron-end

--- front-end/web/src/Dashboard/index.js
+++ front-end/web/src/Dashboard/index.js
@@ -1,13 +1,16 @@
-import React from "react";
 import { useLocalState } from "../util/useLocalStorage";
-
+import React, { useEffect, useState } from "react";
+import Typography from "@mui/material/Typography";
+import PersistentDrawerLeft from "../Sidebar/index.js";
+import { DrawerHeader } from "../Sidebar/DrawerHeader";
+import { width } from "@mui/system";
 const Dashboard = () => {
   const [jwt, setJwt] = useLocalState("", "jwt");
+
   return (
-    <div>
-      <h1> Hello World! </h1>
-      <div> JWT Value is {jwt} </div>
-    </div>
+      <PersistentDrawerLeft>
+          <div> JWT token: {jwt} </div>
+      </PersistentDrawerLeft>
   );
 };
 
